0df525967ae142f20f9b802cfb49fc8089cebc68
Tomoki Omama <tomama0920@gmail.com>
2022-02-14 23:15:14 +0900

    Remove example

--- databucket/databucket_test.py
+++ databucket/databucket_test.py
@@ -18,22 +18,5 @@ class DataBcuketTest(unittest.TestCase):
         self.assertEqual(db._load_bucketpath(), expected)
 
 
-def example():
-
-    j1820 = db.Bucket(object="MAXI J1820+070")
-
-    # Event
-    event = j1820.request_event(obsid="1200120106")
-    df = event.to_pandas()
-    print(df)
-
-    # Curve
-    table = j1820.request_curve(obsid="1200120106", dt=1e3,
-                                energy_range_kev=[0.5, 10],
-                                save_to="../fits/")
-    df = table.to_pandas()
-    print(df)
-
-
 if __name__ == "__main__":
     unittest.main()
