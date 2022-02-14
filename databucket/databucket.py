18116c400edc3a4494b63a532fb8ef3864f0f990
Tomoki Omama <tomama0920@gmail.com>
2022-02-14 23:47:50 +0900

    Modefy

--- databucket/databucket.py
+++ databucket/databucket.py
@@ -29,6 +29,13 @@ def _acquire_dirname(object_name: str):
     return dirname
 
 
+def _must_run_xselect(path_to_file, clobber):
+    isexist = _check_existence(path_to_file)
+    if (clobber is True) or (isexist is False):
+        return True
+    return False
+
+
 class DataBucket():
     """
     """
@@ -39,13 +46,37 @@ class DataBucket():
 
         path_to_bucket = _load_bucketpath()
         dirname = _acquire_dirname(target_name)
-        self.path_to_data = "/".join([path_to_bucket, dirname])
+        self.path_to_object = "/".join([path_to_bucket, dirname])
 
     def request_curve(self, obsid: str, dt: float,
-                      energy_range: list, save_to: str = None):
-        table = Table.load()
+                      energy_range: list, save_to: str = None,
+                      clobber: bool = False):
+        """
+        """
+        _check_obsid(self.target_name, obsid)
+
+        dt_exp = "{:.1e}".format(dt)
+        enrange = _conver_enrange(energy_range_kev)
+        filename = "dt%s_enrange%skev.lc".format(dt_exp, enrange)
+        path_to_file = "/".join([self.path_to_object, obsid, filename])
+
+        mustrun = _must_run_xselect(path_to_file, clobber)
+        if mustrun is True:
+            _run_xselect()
+
+        table = Table.load(path_to_file, format="fits", hdu=1)
+
         return table
 
-    def request_event(self, obsid: str):
+    def request_event(self, obsid: str, clobber: bool = False):
+        """
+        """
+        _check_obsid(self.target_name, obsid)
+
+        filename = "event.evt.gz"
+        path_to_file = "/".join([self.path_to_object, obsid, filename])
+
+        table = Table.load(path_to_file, format="fits", hdu=1)
         table = Table.load()
+
         return table
